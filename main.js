5ab5b7ec35cec1682f4509f28fb8b81944458aac
EWG <edwin.wagner@ehrenberg.de>
2021-05-11 10:13:46 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const character = document.querySelector('.filter');
+
+const filterContent = ["3", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+filterContent.map((item)=>{
+    const span = document.createElement("span");
+    span.textContent = item;
+    span.classList.add('button');
+    character.appendChild(span);
+})
+
+// const elem = document.querySelector('.grid');
+// var iso = new Isotope( elem, {
+//   // options
+//   itemSelector: '.category',
+//   layoutMode: 'fitRows'
+// });
+
+// // element argument can be a selector string
+// //   for an individual element
+// var iso = new Isotope( '.grid', {
+//   // options
+// });
